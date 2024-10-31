9c5d9cd9e6c7121f41807ad384d1492dc51b4778
Hridoy Islam <hridoy4t@gmail.com>
2024-10-31 09:07:00 -0700

    company show solved

--- src/components/shared/user-list.tsx
+++ src/components/shared/user-list.tsx
@@ -39,24 +39,26 @@ export default function UserList({ user, filteredUsers }) {
           </div>
         </Button>
       </Link>
-      <Link to={`/dashboard/task/${companyData?._id}`}>
-        <Button variant="ghost" className="mb-2 w-full justify-start">
-          <Avatar className="mr-2 h-6 w-6 rounded-full">
-            <AvatarFallback>
-              {companyData?.name
-                .split(' ')
-                .map((n) => n[0])
-                .join('')}
-            </AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col ">
-            <span className="text-left">{companyData?.name}</span>
-            <span className="text-left text-xs text-gray-500">
-              {companyData?.role}
-            </span>
-          </div>
-        </Button>
-      </Link>
+      {(user?.role === 'creator' || user?.role === 'user') && (
+        <Link to={`/dashboard/task/${companyData?._id}`}>
+          <Button variant="ghost" className="mb-2 w-full justify-start">
+            <Avatar className="mr-2 h-6 w-6 rounded-full">
+              <AvatarFallback>
+                {companyData?.name
+                  .split(' ')
+                  .map((n) => n[0])
+                  .join('')}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col ">
+              <span className="text-left">{companyData?.name}</span>
+              <span className="text-left text-xs text-gray-500">
+                {companyData?.role}
+              </span>
+            </div>
+          </Button>
+        </Link>
+      )}
       {filteredUsers.map((user) => (
         <Link key={user.id} to={`/dashboard/task/${user?._id}`}>
           <Button variant="ghost" className="mb-2 w-full justify-start">
