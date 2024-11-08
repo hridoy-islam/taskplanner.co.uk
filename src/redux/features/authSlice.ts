349dc74cbc18eb8bc8cf7f7ba20e183d6e2aee89
Mr. Hasan <hasanshahriar32@gmail.com>
2024-11-08 19:34:38 +0600

    add chat logic hooks, implement mobile detection, and update package dependencies

--- src/redux/features/authSlice.ts
+++ src/redux/features/authSlice.ts
@@ -58,7 +58,13 @@ export const loginUser = createAsyncThunk<UserResponse, UserCredentials>(
   async (userCredentials) => {
     const request = await axios.post(
       `${import.meta.env.VITE_API_URL}/auth/login`,
-      userCredentials
+      userCredentials,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json' //this line solved cors
+        }
+      }
     );
     const response = await request.data;
 
